d299f5a297e34804e505d1fba0b81276732d12a9
渣臻 <qz519189636@qq.com>
2018-01-22 12:59:00 +0800

    qz commit

---
+++ js/scroll-to-top.js
@@ -0,0 +1,22 @@
+/**
+ * Created by qinzhen on 2018/1/22.
+ */
+//滚动条移动
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 200) {
+            $('.back-to-top').css("top", '-90px');
+        } else {
+            $('.back-to-top').css("top", '-900px');
+        }
+    });
+});
+
+
+//鼠标点击时
+$(document).ready(function () {
+    $('.back-to-top').click(function () {
+        $('body,html').animate({scrollTop: 0}, 500)
+        $('.back-to-top').css("top", '-900px');
+    })
+})
