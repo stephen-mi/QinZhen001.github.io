fcc84453f0f5f57bbaff7c6728585ffb78f61593
渣臻 <qz519189636@qq.com>
2018-01-22 23:01:25 +0800

    qz commit

--- js/spig.js
+++ js/spig.js
@@ -40,9 +40,15 @@ jQuery(document).ready(function ($) {
 //滚动条移动
 jQuery(document).ready(function ($) {
     var f = parseInt($(".spig").css("top"));
-    $(window).scroll(function () {
+    var currentTop = 0;
+    $(window).scrollStart(function () {
+        currentTop = $(window).scrollTop();
+        console.log('currentTop', currentTop);
+    })
+    $(window).scrollEnd(function () {
         $("#spig").animate({
-                top: $(window).scrollTop() + f
+                top: $(window).scrollTop() - currentTop + f
+                //有bug 要记录当前滚动条的位置
             },
             {
                 queue: false,
